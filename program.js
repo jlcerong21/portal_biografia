685616150413d3a74657a0c499d24b7516127175
Jose Ceron <jlcerong21@gmail.com>
2026-02-03 20:46:25 -0500

    5

--- program.js
+++ program.js
@@ -1,6 +1,7 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([4.628056, -74.065278], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+var marker = L.marker([4.628056, -74.065278]).addTo(map);
